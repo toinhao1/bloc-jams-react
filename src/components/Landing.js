e3218362f5a0bf91bb154c4b34c8023c9eae3573
toinhao1 <toinhao89@gmail.com>
2018-09-03 10:41:34 -0500

    Checkpoint 3 routing complete

---
+++ src/components/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="landing">
+    Landing page will go here
+  </section>
+);
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Library = () => (
+  <section className="library">
+    Library will go here
+  </section>
+);
+export default Library;
