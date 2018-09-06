1d682312f42f0d28a208a47ee1580f79db818e6d
toinhao1 <toinhao89@gmail.com>
2018-09-05 19:14:41 -0500

    Checkpoint 7 audio play: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,8 +9,37 @@ class Album extends Component {
       return album.slug === this.props.match.params.slug
     });
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+    this.audioElement = document.createElement('audio');
+    this.audioElement.rc = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false })
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render(){
@@ -32,7 +61,7 @@ class Album extends Component {
           </colgroup>
           {
             this.state.album.songs.map( (song, index) =>
-                <tbody key={ index }>
+                <tbody key={ index } onClick={() => this.handleSongClick(song)} >
                   <tr>{ index + 1 }</tr>
                   <tr>{ song.title }</tr>
                   <tr>{song.duration}</tr>
