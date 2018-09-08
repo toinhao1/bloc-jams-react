2f333e070f3835c29de8ffacaf771bd5378d6999
toinhao1 <toinhao89@gmail.com>
2018-09-08 09:58:55 -0700

    Assignment 8 playerbar: complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -51,6 +51,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(4, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   render(){
     return (
       <section className="album">
@@ -98,6 +106,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
